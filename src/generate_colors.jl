a4b7dd78c858db5dc2da34a5ad1c93c10a083a9a
brsan <brsandvik@gmail.com>
2023-05-20 10:37:24 -0700

    fix

--- src/generate_colors.jl
+++ src/generate_colors.jl
@@ -3,9 +3,9 @@
 end
 
 # Feel free to edit these numbers!
-number_of_colors_generated = 10
+number_of_colors_generated = 20
 minimum_iterations = 10000000
-minimum_iterations_since_last_improvement = 100000000
+minimum_iterations_since_last_improvement = 1000000
 
 begin
     so_colors = so_distinguishable_colors(number_of_colors_generated; its=minimum_iterations, thresh=minimum_iterations_since_last_improvement)
